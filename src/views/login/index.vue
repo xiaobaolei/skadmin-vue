37f5f8c6e4623a8961f0cc8ffe36c71c7698a678
ZJDM <dengsinkiang@gmail.com>
2019-05-21 16:15:06 +0800

    photo

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -103,7 +103,7 @@ export default {
     justify-content: center;
     align-items: center;
     height: 100%;
-    background-image:url(../../assets/images/top_images/logo2.jpg);
+    background-image:url(../../assets/images/top_images/login.jpg);
     background-size: cover;
   }
   .title {
